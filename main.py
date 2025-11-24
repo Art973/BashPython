a31b98812a555c7009a0df9b2de3c41b63bcccb1
artfan973 <artfan973@gmail.com>
2025-11-24 12:16:55 +0000

    12312312322

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-while(c:=input('> '))not in['exit','quit']:print(__import__('os').popen(c).read())
+import subprocess
+import os
+
+cwd = os.getcwd()
+while (cmd := input("> ")) not in ("exit", "quit"):
+    if cmd.startswith("cd "):
+        path = cmd[3:].strip()
+        try:
+            cwd = os.path.abspath(os.path.join(cwd, path))
+            os.chdir(cwd)  # обновляем для текущего процесса Python
+        except Exception as e:
+            print(f"cd error: {e}")
+    else:
+        result = subprocess.run(cmd, shell=True, cwd=cwd, text=True, capture_output=True)
+        print(result.stdout, end="")
+        if result.stderr:
+            print(result.stderr, end="")
